7f3dcecbcf5e081c3ee381c2633b5f8b5373ccd9
HKemper1 <122270139+HKemper1@users.noreply.github.com>
2023-04-25 16:23:29 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,109 @@
+<?php include_once "php/head.php" ?>
+
+<body>
+
+    <?php include_once "php/nav.php" ?>
+
+    <main class="container">
+
+        <section>
+
+            <h2 class="h2"> Local Goods Oldenburg</h2>
+
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2392.9642307156832!2d8.18054371310003!3d53.14673648952342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b6dfca0f089dff%3A0x283f06b42b139285!2sCarl%20von%20Ossietzky%20Universit%C3%A4t%20Oldenburg!5e0!3m2!1sde!2sde!4v1682415771134!5m2!1sde!2sde" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<h2>Lebensmittel</h2>
+            <div>
+                <span>Titel</span>
+                <span>Kategorie</span>
+                <span>Standort</span>
+                <span>E-Mail</span>
+                <span>Datum</span>
+            </div>
+
+            <div>
+                <div>
+                    <span><a href="lebensmittel.php">Titel 1</a></span>
+                    <span>Kategorie1</span>
+                    <span>Standort1</span>
+                    <span>EMail 1</span>
+                    <span>Datum 1</span>
+                </div>
+                <div>
+                    <span><a href="lebensmittel.php">Titel 2</a></span>
+                    <span>Kategorie2</span>
+                    <span>Standort2</span>
+                    <span>EMail 2</span>
+                    <span>Datum 2</span>
+                </div>
+                <div>
+                    <span><a href="lebensmittel.php">Titel 3</a></span>
+                    <span>Kategorie3</span>
+                    <span>Standort3</span>
+                    <span>EMail 3</span>
+                    <span>Datum 3</span>
+                </div>
+                <div>
+                    <span><a href="lebensmittel.php">Titel 4</a></span>
+                    <span>Kategorie4</span>
+                    <span>Standort4</span>
+                    <span>EMail 4</span>
+                    <span>Datum 4</span>
+                </div>
+            </div>
+        <div>
+            <a href="angebote-neu.php">Neues Lebensmittel</a>
+        </div>
+
+            <h2>Maschinen</h2>
+            <div>
+                <span>Titel</span>
+                <span>Kategorie</span>
+                <span>Standort</span>
+                <span>E-Mail</span>
+                <span>Datum</span>
+            </div>
+
+            <div>
+                <div>
+                    <span><a href="maschinen.php">Titel 1</a></span>
+                    <span>Kategorie1</span>
+                    <span>Standort1</span>
+                    <span>EMail 1</span>
+                    <span>Datum 1</span>
+                </div>
+                <div>
+                    <span><a href="maschinen.php">Titel 2</a></span>
+                    <span>Kategorie2</span>
+                    <span>Standort2</span>
+                    <span>EMail 2</span>
+                    <span>Datum 2</span>
+                </div>
+                <div>
+                    <span><a href="maschinen.php">Titel 3</a></span>
+                    <span>Kategorie3</span>
+                    <span>Standort3</span>
+                    <span>EMail 3</span>
+                    <span>Datum 3</span>
+                </div>
+                <div>
+                    <span><a href="maschinen.php">Titel 4</a></span>
+                    <span>Kategorie4</span>
+                    <span>Standort4</span>
+                    <span>EMail 4</span>
+                    <span>Datum 4</span>
+                </div>
+            </div>
+        </section>
+
+
+        <div>
+            <a href="angebote-neu.php">Neue Maschinen</a>
+        </div>
+        </section>
+    </main>
+
+    <?php include_once "php/footer.php" ?>
+</body>
+
+</html>
